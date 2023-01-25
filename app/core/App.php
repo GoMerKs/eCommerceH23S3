44cc0c9164f0ec283049bbf99db23fa2ccd4335b
Michel Paquette <paquettm@vaniercollege.qc.ca>
2023-01-25 11:50:45 -0500

    Introduction

---
+++ app/core/App.php
@@ -0,0 +1,20 @@
+<?php
+
+class App{
+
+	function __construct(){
+		//this is where we want to route the requests to the appropriate classes/methods
+
+		echo $_GET['url'] ?? 'No url provided';
+
+		$request = $this->parseUrl($_GET['url']);
+
+		var_dump($request);
+	}
+
+	function parseUrl($url){
+		return explode('/', $url);
+	}
+
+
+}
