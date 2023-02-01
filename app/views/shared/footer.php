878833e678af406fe30b49fc50419696b463957d
Michel Paquette <paquettm@vaniercollege.qc.ca>
2023-02-01 11:41:13 -0500

    Completed bootstrapping

---
+++ app/views/shared/footer.php
@@ -0,0 +1,6 @@
+	<a href='/Main/logUser'>Write your name in the log book</a>
+	<a href='/Main/index'>Index</a>
+	<a href='/Main/index2'>Index2</a>
+	<a href='/Main/Greetings/Bob'>Greet Bob</a>
+</body>
+</html>
